dd10adbdd4b04ac9cd996a109efca2d06ea3e848
Luke Sanderlin <luke.sanderlin@gmail.com>
2016-07-27 14:36:43 -0400

    initial trial script

--- install.sh
+++ install.sh
@@ -1,2 +1,15 @@
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh --interactive
 
-echo $BASH_IT
+echo "using --> BASH_IT VAR:$BASH_IT"
+
+source ~/.bashrc
+
+mkdir -p $BASH_IT/custom/themes/shakes
+ln -s -f ~/bash-it-sync/shakes.theme.bash $BASH_IT/custom/themes/shakes/shakes.theme.bash
+ln -s -f ~/bash-it-sync/custom.bash $BASH_IT/lib/custom.bash
+ln -s -f ~/bash-it-sync/custom.aliases.bash $BASH_IT/aliases/custom.aliases.bash
+
+source ~/.bashrc
+
+install_custom_support
