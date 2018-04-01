db21293a56e5bc3d09dd48371d970f5cc6edcf41
Luke Sanderlin <luke.sanderlin@gmail.com>
2018-04-01 19:25:01 +0000

    update.sh pull is now quiet

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
-if git diff-index --quiet HEAD --; then
+if [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | sed 's/\// /g') | cut -f1) ]; then
     echo "No changes to bash-it-sync"
 else
     echo "Changes to bash-it-sync have been detected, syncing..."
-    git pull
+    git pull -q
     # source ~/.bashrc
 fi
+
